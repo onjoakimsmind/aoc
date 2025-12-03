49c62366d2f7c989c942bb9732e24d174e4089e8
Joakim Stenberg <joakim@stenberg.uk>
2025-12-03 23:42:16 +0100

    solution 2015/06

--- app/Solutions/2015/06/A.php
+++ app/Solutions/2015/06/A.php
@@ -15,14 +15,48 @@ class A
         $this->inputData = trim($inputData);
     }
 
-    public function solve(): int
+    public function solve(?string $input = null): int
     {
-        // TODO: Implement part A solution
-        return 0;
+        $input = $input ?? $this->inputData;
+        $commands = explode("\n", trim($input));
+
+        $grid = array_fill(0, 1000000, 0);
+        foreach ($commands as $command) {
+            if (!preg_match('/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/', $command, $m)) {
+                continue;
+            }
+
+            [$full, $action, $x1, $y1, $x2, $y2] = $m;
+
+            $x1 = (int)$x1;
+            $y1 = (int)$y1;
+            $x2 = (int)$x2;
+            $y2 = (int)$y2;
+
+            for ($x = $x1; $x <= $x2; $x++) {
+                $rowStart = $x * 1000;
+
+                for ($y = $y1; $y <= $y2; $y++) {
+                    $i = $rowStart + $y;
+
+                    if ($action === "turn on") {
+                        $grid[$i] = 1;
+                    } elseif ($action === "turn off") {
+                        $grid[$i] = 0;
+                    } else {
+                        $grid[$i] ^= 1;
+                    }
+                }
+            }
+        }
+
+        return array_sum($grid);
     }
 
     public function test(TestRunner $t, string $testInput): void
     {
-        $t->assertEquals(0, $this->solve(), 'Part A');
+        $t->assertEquals(1000000, $this->solve("turn on 0,0 through 999,999"), 'Part A (example 1)');
+        $t->assertEquals(1000, $this->solve("toggle 0,0 through 999,0"), 'Part A (example 2)');
+        $t->assertEquals(999996, $this->solve("turn on 0,0 through 999,999\nturn off 499,499 through 500,500"), 'Part A (example 3)');
     }
 }
--- app/Solutions/2015/06/B.php
+++ app/Solutions/2015/06/B.php
@@ -15,14 +15,49 @@ class B
         $this->inputData = trim($inputData);
     }
 
-    public function solve(): int
+    public function solve(?string $input = null): int
     {
-        // TODO: Implement part B solution
-        return 0;
+        $input = $input ?? $this->inputData;
+        $commands = explode("\n", trim($input));
+
+        $grid = array_fill(0, 1000000, 0);
+        foreach ($commands as $command) {
+            if (!preg_match('/(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)/', $command, $m)) {
+                continue;
+            }
+
+            [$full, $action, $x1, $y1, $x2, $y2] = $m;
+
+            $x1 = (int)$x1;
+            $y1 = (int)$y1;
+            $x2 = (int)$x2;
+            $y2 = (int)$y2;
+
+            for ($x = $x1; $x <= $x2; $x++) {
+                $rowStart = $x * 1000;
+
+                for ($y = $y1; $y <= $y2; $y++) {
+                    $i = $rowStart + $y;
+
+                    if ($action === "turn on") {
+                        $grid[$i] += 1;
+                    } elseif ($action === "turn off") {
+                        if ($grid[$i] > 0) {
+                            $grid[$i] -= 1;
+                        }
+                    } else {
+                        $grid[$i] += 2;
+                    }
+                }
+            }
+        }
+
+        return array_sum($grid);
     }
 
     public function test(TestRunner $t, string $testInput): void
     {
-        $t->assertEquals(0, $this->solve(), 'Part B');
+        $t->assertEquals(1, $this->solve("turn on 0,0 through 0,0"), 'Part B (example 1)');
+        $t->assertEquals(2000000, $this->solve("toggle 0,0 through 999,999"), 'Part B (example 2)');
     }
 }
